c240fc971fb6ecb216ee3eddbfa2eae3b7a8f31b
Aswathi C <73010029+webapplicationdevelopmentlab@users.noreply.github.com>
2023-12-27 18:19:43 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../style/style.css'
+
+export default function Game() {
+  return (
+    <div className='head'>
+      <h1>Epic Games: An American Video Game And Software Developer And Publisher Based In Cary,North Carolina.</h1>
+      <h3>Create,play and battle with friends for free in Fortnite.Be the last player standing in Battle Royale and Zero Build,experience a concert or live event,or discover over a million creator made games,including racing,parkour,zombie survival and more.Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends.Find it all in Fortnite...Drop In.</h3>
+      <button className='btn'>Visit Website</button>
+      <h3>Explore Large,Distructible Environments Where No Two Games Are Ever The Same.</h3>
+      <h3>Team Up With Friends By Sprinting,Climbing And Smashing Your Way To Earn Your Victory Royale.</h3>
+      <h3>Discover Even More Ways To Play Across Thousands Of Creator-Made Game Genres.</h3>
+      <h2>Our Contribution</h2>
+      <h3>Our core offering extends beyond mere profit generation;we empathize the growth and active involvement of our user community.Collaborating with us represents an investment,rather than mere an expenditure.Our dedication to providing distinctive digital interactions guarantees unparalleled benefits for our clientele.</h3>
+      <h1>5M       $500K        10X</h1>
+      <h3>Daily User Engagements</h3>
+      <h3>Revenue Surge for an Platform</h3>
+      <h3>ROAS on all our marketing campaigns</h3>
+      <h2>Interested In Delving Deeper In To Our Project? </h2>
+      <h3>If you'd like to explore further details about our initiatives or any of our affiliated brands,don't hesitate to connect.You can reach out to us via email at <b>hello@abc.com</b>or give us a call at <b>+91 480 20802730</b> </h3>
+      <button className='btn'>Ring us on skype</button>
+      <button className='btn'>Contact Us</button>
+      <footer>2019-2023 abc Technology Solutions Pvt.Ltd. All Rights Reserved</footer>
+    </div>
+  )
+}
+
